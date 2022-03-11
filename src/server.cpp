839aedc09f5b85f10c5ca36cc194d37ca948f553
arshiatehrani <arshia.tehrani1380@gmail.com>
2022-03-11 20:32:42 +0330

    2 server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -1,9 +1,9 @@
 #include "server.h"
 std::shared_ptr<Client> Server::add_client(std::string id)
 {
-    for (const auto& [name, number] : clients) {
+    for (const auto& [name, value] : clients) {
         const std::strong_ordering order = id <=> (*name).get_id();
-        if (std::is_eq()) {
+        if (std::is_eq(order)) {
             std::random_device rd;
             std::mt19937 mt(rd());
             std::uniform_real_distribution<double> dist(1000, 9999);
@@ -14,7 +14,27 @@ std::shared_ptr<Client> Server::add_client(std::string id)
     }
 
     double wallet { 5 };
-    std::shared_ptr<Client> pntr_client { std::make_shared<client>(id) };
+    std::shared_ptr<Client> pntr_client { std::make_shared<client>(id, *this) };
     clients[pntr_client] = wallet;
     return pntr_client;
+}
+std::shared_ptr<Client> Server::get_client(std::string id)
+{
+    for (const auto& [name, value] : clients) {
+        const std::strong_ordering order = id <=> (*name).get_id();
+        if (std::is_eq(order)) {
+            return name;
+        }
+    }
+    throw std::logic_error("Client not found!");
+}
+double Server::get_wallet(std::string id)
+{
+    for (const auto& [name, value] : clients) {
+        const std::strong_ordering order = id <=> (*name).get_id();
+        if (std::is_eq(order)) {
+            return value;
+        }
+    }
+    throw std::logic_error("Client not found!");
 }
