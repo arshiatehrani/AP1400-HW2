a836d77b8f6c7d53159356cab7e5a3756200f5ed
arshiatehrani <arshia.tehrani1380@gmail.com>
2022-03-14 21:16:51 +0330

    all sources that all clients can mine! // without couts

--- src/server.cpp
+++ src/server.cpp
@@ -118,27 +118,3 @@ void show_wallets(const Server& server)
         std::cout << client.first->get_id() << " : " << client.second << std::endl;
     std::cout << std::string(20, '*') << std::endl;
 }
-// extracting sender
-// for (size_t j {}; j < pending_trxs.size(); j++) {
-//     parse_trx(pending_trxs[j], sender[j], receiver[j], value[j]);
-// }
-
-// while (true) {
-//     for (size_t k {}; k < pending_trxs.size(); k++) {
-//         size_t nonce { get_client(sender[k])->generate_nonce() };
-//         hash = crypto::sha256(mempool + std::to_string(nonce));
-//         if (hash.substr(0, 10).find("000") != std::string::npos) {
-//             for (size_t x {}; x < pending_trxs.size(); x++) {
-//                 clients[get_client(sender[x])] -= value[x];
-//                 clients[get_client(receiver[x])] += value[x];
-//             }
-//             std::cout << "********Wallet before mine: " << clients[get_client(sender[k])] << std::endl;
-//             clients[get_client(sender[k])] += 6.25;
-//             std::cout << "********Wallet after mine: " << clients[get_client(sender[k])] << std::endl;
-//             pending_trxs.clear();
-//             std::cout << "********Miner's ID: " << sender[k] << std::endl;
-//             std::cout << "********Hash: " << hash << std::endl;
-//             return nonce;
-//         }
-//     }
-// }
